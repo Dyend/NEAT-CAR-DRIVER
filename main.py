db768c68060e31f25d570b3bdbb5afe60c6a1a78
DGJumpa <jpraabm@hotmail.com>
2022-05-19 18:00:59 -0400

    Añadido movimiento en carrito y en objeto a evadir.

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ xml_path = './models/autito.xml'
 model = load_model_from_path(xml_path)
 sim = MjSim(model)
 viewer = MjViewer(sim)
+t = 0
 while True:
     sim.step()
     viewer.render()
+    sim.data.ctrl[1] = 1
+    sim.data.ctrl[0] = -1
+    sim.data.qpos[15] = 0.5 + math.cos(t*0.01)
+    t+=1
+    print(sim.data.ctrl)
+    print(sim.data.qpos)
+    print(sim.get_state())
