3ed2c70c8d93a8932df28ee1486bcc443a9ee2f3
Jose <alonso__96@hotmail.es>
2022-05-19 23:09:08 -0400

    agregado input/output para la red

--- main.py
+++ main.py
@@ -7,14 +7,40 @@ xml_path = './models/autito.xml'
 model = load_model_from_path(xml_path)
 sim = MjSim(model)
 viewer = MjViewer(sim)
-t = 0
-while True:
-    sim.step()
-    viewer.render()
-    sim.data.ctrl[1] = 1
-    sim.data.ctrl[0] = -1
-    sim.data.qpos[15] = 0.5 + math.cos(t*0.01)
-    t+=1
-    print(sim.data.ctrl)
-    print(sim.data.qpos)
-    print(sim.get_state())
+
+def simular(steps, render=False):
+    t = 0
+    fitness = 0
+    for step in range(steps):
+        sim.step()
+        if render:
+            viewer.render()
+        sensor_proximidad = sim.data.get_sensor("rangefinder")
+        sensor_giroscopio = sim.data.get_sensor("gyro")
+        sensor_velocimetro = sim.data.get_sensor("velocimeter")
+        sensor_acelerometro = sim.data.get_sensor("accelerometer")
+        #Entradas
+        input = [sensor_proximidad, sensor_giroscopio, sensor_acelerometro, sensor_velocimetro]
+        # Salidas
+        output = [0, 0]
+        for i in range(output):
+            sim.data.ctrl[i] = output[i]
+
+        sim.data.qpos[15] = 0.5 + math.cos(t*0.01)
+        t += 1
+        estado = sim.get_state()
+        fitness += evaluar(estado)
+
+    return fitness
+
+def entrenamiento():
+    episodes = 1
+    steps = 3000
+    for e in range(episodes):
+        resultado = simular(steps, render=True)
+
+def evaluar(estado):
+    return 1
+
+entrenamiento()
+
