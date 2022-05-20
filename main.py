33232ea8b1b279aeb0cfd3f1623db79ea0598c58
DGJumpa <jpraabm@hotmail.com>
2022-05-20 13:06:38 -0400

    Añadida función de fitness inicial. Descuenta puntos por retroseder. Detecta colisiones y en caso de haber una colision retorna 0 puntaje hasta dejar de colisionar. PS: Hay que evaluar que tanto puntaje quitar en caso de haber una colisión.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from mujoco_py import load_model_from_xml, MjSim, MjViewer, load_model_from_path
 import math
 import os
@@ -29,7 +30,8 @@ def simular(steps, render=False):
         sim.data.qpos[15] = 0.5 + math.cos(t*0.01)
         t += 1
         estado = sim.get_state()
-        fitness += evaluar(estado)
+        # fitness += evaluar(estado)
+        fitness += evaluar(sim.data.ctrl[1], sim.data.ncon)
 
     return fitness
 
@@ -39,8 +41,20 @@ def entrenamiento():
     for e in range(episodes):
         resultado = simular(steps, render=True)
 
-def evaluar(estado):
-    return 1
+def evaluar(velocidad, datos_colision):
+    if (detectar_colision(datos_colision)):
+        #Retornar valores negativos si se desea descontar puntaje por cada frame que se está haciendo colisión.
+        return 0
+    #retornamos como maximo el valor 1 correspondiende a la velocidad.
+    #se descuenta puntaje si este está retrosediendo (velocidad negativa) pues se considera que estaria pasando por un lugar que ya visitó.
+    return velocidad
 
+def detectar_colision(datos_colision):
+    for i in range(datos_colision):
+        contact = sim.data.contact[i]
+        if(sim.model.geom_id2name(contact.geom1) == None or sim.model.geom_id2name(contact.geom2) == None):
+            if(sim.model.geom_id2name(contact.geom1) != "floor" or sim.model.geom_id2name(contact.geom2) == "floor"):
+                return True
+    return False
 entrenamiento()
 
